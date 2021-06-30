b21d87347755494d6abc9515f2de39fa21f7d143
ktr92 <kudashevtim@gmail.com>
2021-06-30 17:54:56 +0300

    start

--- js/main.js
+++ js/main.js
@@ -1,142 +1,139 @@
 $(document).ready(function() {
 
-    $(function() {
-        $("input[type=checkbox]").click(function(){
-            if ($(this).closest('form').find('input[type=checkbox]:checked').length == 0) {
-                $(this).closest('form').find('[type=submit]').prop('disabled', true);
-            }
-            else {
-                 $(this).closest('form').find('[type=submit]').prop('disabled', false);
-            }
-        });
+   $(".productslider__slider").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+    dots: false,
+    prevArrow: $(".productslider__arrow_left"),
+    nextArrow: $(".productslider__arrow_right"),
+     responsive: [
+    {
+      breakpoint: 1023,
+      settings: {
+         slidesToShow: 3,
+    slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+         slidesToShow: 3,
+    slidesToScroll: 1,
+      }
+    },    
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+  });
+
+var counterTeaserL = $('.stats');
+var winHeight = $(window).height();
+if (counterTeaserL.length) {
+    var firEvent = false,
+        objectPosTop = $('.stats').offset().top;
+
+        //when element shows at bottom
+        var elementViewInBottom = objectPosTop - winHeight;
+    $(window).on('scroll', function() {
+        var currentPosition = $(document).scrollTop();
+        //when element position starting in viewport
+      if (currentPosition > elementViewInBottom && firEvent === false) {
+        firEvent = true;
+        animationCounter1();
+        animationCounter2();
+        animationCounter3();
+        
+      }   
     });
+}
 
-    $(document).keyup(function(e) {
-        if(e.keyCode == 27) {
-           
-            $('.search__full_header').show();
-            $('.s-overlay').removeClass('active_search');
-            $('.mainmenu ').slideUp();
-            $('.menubutton').removeClass('active');
-        };
-    });
-
-    $('.menubutton').on('click', function() {
-        $(this).toggleClass('active');
-        $('.search__full_header').toggle();
-        $('.s-overlay').toggleClass('active_search');
-        $('.mainmenu ').slideToggle();
-        //$('.s-overlay').toggleClass('active_headertop');
-    });
-
-     $('.s-overlay').on('click', function() {
-        $(this).toggleClass('active_search');
-        $('.search__full_header').toggle();
-        $('.mainmenu ').slideToggle();
-        $('.menubutton').toggleClass('active');
-        });
-
- 
+//counter function will animate by using external js also add seprator "."
+ function animationCounter1(){
+         $('#num_1').each(function () {
+          $(this).prop('Counter', 0).animate({
+                  Counter: $(this).data('value')
+              }, {
+              duration: 1200,
+              easing: 'swing',
+              step: function (now) {                      
+                  $(this).text(this.Counter.toFixed(0));
+              }
+            });
+          });
+    }
 
 
-    $('.categories__button').on('click', function() {
-        $(this).toggleClass('active');
-        $(this).siblings('.categories__items').slideToggle();
-     
-    });
-
-    $('.newssort__current').on('click', function() {
-        $(this).toggleClass('active');
-        $(this).siblings('.newssort__items').slideToggle();
-     
-    });
-
-    $('.input.search__input').on('click', function() {
-        $(this).removeAttr('placeholder');
-     
-    });
-
-    
+ function animationCounter2(){
+         $('#num_2').each(function () {
+          $(this).prop('Counter', 0).animate({
+                  Counter: $(this).data('value')
+              }, {
+              duration: 1900,
+              easing: 'swing',
+              step: function (now) {                      
+                  $(this).text(this.Counter.toFixed(0));
+              }
+            });
+          });
+    }
 
 
-   
-    jQuery.fn.posDesktop = function () {
-       var elem = $(this).attr('data-id');
-       var offset = $(this).offset();
-       var diff_out = $(this).outerHeight();
-      
-
-        var type = $('.asidecols__aside').find('[data-id='+elem+']').attr("data-position");
-        console.log(type);
-
-        if (type=="top") {
-            $('.asidecols__aside').find('[data-id='+elem+']').offset({top: offset.top + diff_out + 25});
-        }
-
-       if (type=="bottom") {
-            var diff_inner = Math.abs($(this).height() - $('.asidecols__aside').find('[data-id='+elem+']').height());
-            $('.asidecols__aside').find('[data-id='+elem+']').offset({top: offset.top - diff_inner});
-        }
-
-       
-       return this;
+ function animationCounter3(){
+         $('#num_3').each(function () {
+          $(this).prop('Counter', 0).animate({
+                  Counter: $(this).data('value')
+              }, {
+              duration: 3200,
+              easing: 'swing',
+              step: function (now) {                      
+                  $(this).text(this.Counter.toFixed(0));
+              }
+            });
+          });
     }
 
 
 
-     if ($(window).width() > 959) { 
-         if ($('[data-id]').length != 0) {
-                $('.asidecols__col').find('[data-id]').each(function() {
-                $(this).posDesktop();
-             });
-            }
-
-     }
-     else {
-        
-     }
 
 
-    $(window).on('resize', function(){
 
-      if ($(window).width() > 959) { 
-         if ($('[data-id]').length != 0) {
-                $('.asidecols__col').find('[data-id]').each(function() {
-                $(this).posDesktop();
-             });
-            }
-
-     }
-     else {
-       
-     }
-    });
-
-
-    $('.accordeon_type1 .accordeon-titlejs').click(function (event) {
-        $(this).closest('.accordeon-js').find('.accordeon-contentjs').not($(this).next()).hide(300).removeClass('active');
-        $(this).closest('.accordeon-js').find('.accordeon-titlejs').not($(this)).removeClass('active');
-        $(this).toggleClass('active');
-        /* $([document.documentElement, document.body]).animate({
-            scrollTop: $(this).offset().top
-        }, 400);*/
-        $(this).next('.accordeon-contentjs').slideToggle();
-    });
-   
-
-    $('.accordeon_type2 .accordeon-titlejs').click(function (event) {
-        $(this).closest('.accordeon-js').find('.accordeon-contentjs').not($(this).next()).removeClass('active');
-        $(this).closest('.accordeon-js').find('.accordeon-titlejs').not($(this)).removeClass('active');
-        $(this).toggleClass('active');
-        /* $([document.documentElement, document.body]).animate({
-            scrollTop: $(this).offset().top
-        }, 400);*/
-        $(this).next('.accordeon-contentjs').toggleClass('active');
-    });
-   
-   
+    function incrementValue(e) {
+        e.preventDefault();
+        var fieldName = $(e.target).data('field');
+        var parent = $(e.target).closest('div');
+        var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+        if (!isNaN(currentVal)) {
+        parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+        } else {
+        parent.find('input[name=' + fieldName + ']').val(1);
+        }
+      }
+      function decrementValue(e) {
+        e.preventDefault();
+        var fieldName = $(e.target).data('field');
+        var parent = $(e.target).closest('div');
+        var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+        if (!isNaN(currentVal) && currentVal > 1) {
+        parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+        } else {
+        parent.find('input[name=' + fieldName + ']').val(1);
+        }
+      }
+      $('.quantity').on('click', '.quantity-plus', function(e) {
+        incrementValue(e);
+      });
+      $('.quantity').on('click', '.quantity-minus', function(e) {
+        decrementValue(e);
+      });
 });
-
 
 
 
