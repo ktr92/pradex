c8fc114d76ee9ae9ff1198a07d42c979373799e8
ktr92 <kudashevtim@gmail.com>
2021-07-14 19:02:12 +0300

    start

--- js/main.js
+++ js/main.js
@@ -106,6 +106,10 @@ if (counterTeaserL.length) {
       $('.searchbutton').click(function(e) {
         $('.headermain__search').slideToggle();
     });
+      $('.closesearch').click(function(e) {
+        $('.headermain__search').slideToggle();
+    });
+      
 
     $('.menubtn-js').click(function(e) {
         $('.bglayer').toggle();
@@ -116,6 +120,12 @@ if (counterTeaserL.length) {
     $('.mainmenu').hide();
     $('.bglayer').hide();
   });
+
+
+   $('.mobilefilter').on('click', function() {
+    $('.filter').slideToggle();
+  });
+
 
 
 
@@ -131,6 +141,18 @@ if (counterTeaserL.length) {
                 $('body').removeClass('fixed');
             }
         });
+       } else {
+          $(window).scroll(function() {
+            scroll = $(window).scrollTop();
+
+            if (scroll >= 136) {
+                $('.header').addClass('fixed');
+                $('body').addClass('fixed');
+            } else {
+                $('.header').removeClass('fixed');
+                $('body').removeClass('fixed');
+            }
+        });
        };
 
 
@@ -138,7 +160,45 @@ if (counterTeaserL.length) {
           $(this).closest('.filter__item').toggleClass('filter__item_open');
           $(this).siblings('.filter__form').slideToggle();
         });
+
+
+if ($(window).width() > 1023) {
+(function(){  // анонимная функция (function(){ })(), чтобы переменные "a" и "b" не стали глобальными
+var a = document.querySelector('#ordertotal__wrapper'), b = null;  // селектор блока, который нужно закрепить
+window.addEventListener('scroll', Ascroll, false);
+document.body.addEventListener('scroll', Ascroll, false);  // если у html и body высота равна 100%
+function Ascroll() {
+  if (b == null) {  // добавить потомка-обёртку, чтобы убрать зависимость с соседями
+    var Sa = getComputedStyle(a, ''), s = '';
+    for (var i = 0; i < Sa.length; i++) {  // перечислить стили CSS, которые нужно скопировать с родителя
+      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+      }
+    }
+    b = document.createElement('div');  // создать потомка
+    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+    a.insertBefore(b, a.firstChild);  // поместить потомка в цепляющийся блок первым
+    var l = a.childNodes.length;
+    for (var i = 1; i < l; i++) {  // переместить во вновь созданного потомка всех остальных потомков (итого: создан потомок-обёртка, внутри которого по прежнему работают скрипты)
+      b.appendChild(a.childNodes[1]);
+    }
+    a.style.height = b.getBoundingClientRect().height + 'px';  // если под скользящим элементом есть другие блоки, можно своё значение
+    a.style.padding = '0';
+    a.style.border = '0';  // если элементу присвоен padding или border
+  }
+  if (a.getBoundingClientRect().top <= 0) { // elem.getBoundingClientRect() возвращает в px координаты элемента относительно верхнего левого угла области просмотра окна браузера
+    b.className = 'sticky'; //Класс для контейнера
+  } else {
+    b.className = '';
+  }
+  window.addEventListener('resize', function() {
+    a.children[0].style.width = getComputedStyle(a, '').width
+  }, false);  // если изменить размер окна браузера, измениться ширина элемента
+}
+})();
+}
 });
+
 
 
 
